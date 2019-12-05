2316bf0e385b8d819245d1e9837fe3c0a99377ad
gsa <glebofff@gmail.com>
2019-12-05 18:16:42 +0500

    known currencies 'enum'

---
+++ currencies/common/strenum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Stanislav Glebov <glebofff@gmail.com> 
+# Created: 05/12/19
+from django.utils.decorators import classproperty
+
+
+class StrEnum(object):
+    __descriptions__ = {}
+
+    @classmethod
+    def description(cls, cur):
+        return cls.__descriptions__.get(cur)
+
+    @classproperty
+    def choices(cls):
+        return tuple((k, v) for k, v in cls.__descriptions__.items())
+
