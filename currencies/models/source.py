d149e0d48ec1cb939813211a450fd2401edd639b
gsa <glebofff@gmail.com>
2019-12-09 13:51:05 +0500

    RateSource model test, fixes

--- currencies/models/source.py
+++ currencies/models/source.py
@@ -2,17 +2,17 @@
 #
 # Author: Stanislav Glebov <glebofff@gmail.com> 
 # Created: 05/12/19
-import datetime
-
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
 class RateSource(models.Model):
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['abbr'], name='rate_source_unique_abbr_cst')
+            models.UniqueConstraint(fields=['abbr'], name='rate_source_unique_abbr_cst'),
+            models.CheckConstraint(check=~models.Q(abbr=""), name="rate_source_not_empty_abbr_cst")
         ]
 
-    abbr = models.CharField(max_length=64, db_index=True)
+    abbr = models.CharField(max_length=64, db_index=True, validators=[MinLengthValidator(1)])
     last_poll = models.DateTimeField(auto_now_add=False, null=True)
     remote_ts = models.DateTimeField(auto_now_add=False, null=True)
--- currencies/tests/models/rate_source.py
+++ currencies/tests/models/rate_source.py
@@ -2,12 +2,22 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from currencies.models import RateSource
+from django.db import IntegrityError
 
 
 class RateSourceModelTestCase(TestCase):
-    def test_instance_default_values(self):
+    def test_default_values(self):
         instance = RateSource()
         self.assertIsNone(instance.last_poll)
         self.assertIsNone(instance.remote_ts)
 
-        self.assertRaises(ValidationError, instance.save)
+    def test_empty_constraint(self):
+        instance = RateSource()
+        self.assertRaises(IntegrityError, instance.save)
+
+    def test_unique_constraint(self):
+        x = RateSource(abbr='ABC')
+        y = RateSource(abbr='ABC')
+
+        x.save()
+        self.assertRaises(IntegrityError, y.save)
